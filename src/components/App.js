f46ac87404662b8ecb9c5ef4d2555b8c9490c10c
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-05-24 15:17:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <input id="name" value={name} onChange={handleNameChange}></input>
         <div id="preview">
           {/* <label>Hello {name}!</label> */}
-          {name && <label>Hello {name}!</label>}
+          {name && <p>Hello {name}!</p>}
         </div>
     </div>
   )
